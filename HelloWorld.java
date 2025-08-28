455dbf6287f4a7e2587b5a9cee326d5ee4832b3a
MuhammadBaibarsZainUlAbideen <baibarsjutt@gmail.com>
2025-08-28 13:24:34 -0600

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class HelloWorld {
+    static void printMessage(int num1, int num2) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter oprator: +, -, *, or /");
+        String userName = myObj.nextLine();  // Read user input
+        if (userName.equals("+")) {
+            System.out.println(num1 + num2);
+        } else if (userName.equals("-")) {
+            System.out.println(num1 - num2);
+        } else if (userName.equals("*")) {
+            System.out.println(num1 * num2);
+        } else if (userName.equals("/")) {
+            System.out.println(num1 / num2);
+        } else {
+            System.out.println("Invalid operator");
+        }
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        printMessage(5, 10);
+    }
+}
